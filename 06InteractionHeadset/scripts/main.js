c8caf5781a6552fd8a28999ecc0caf4a9b6f767a
smengarduque <76743403+smengarduque@users.noreply.github.com>
2022-09-16 22:31:55 +0200

    Update main.js

--- 06InteractionHeadset/scripts/main.js
+++ 06InteractionHeadset/scripts/main.js
@@ -34,7 +34,7 @@ function grow(){
  	myOtherBox.object3D.scale.x *= growspeed;
  	myOtherBox.object3D.scale.y *= growspeed;
  	myOtherBox.object3D.scale.z *= growspeed;
- 	//console.log(myOtherBox.object3D.scale);
+ 	console.log(myOtherBox.object3D.scale);
  }
 
 myOtherBox.addEventListener('click', function(){ // uses a fuse
